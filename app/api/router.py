bc396f81edac4b8bd2f0d54be76b61c4949a797c
lysuhher <firdevszsa@gmail.com>
2026-07-25 15:41:15 +0300

    feat: implement inventory management API

--- app/api/router.py
+++ app/api/router.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from app.api.routes import health, orders, root, products
+from app.api.routes import health, orders, root, products, inventory
 
 api_router = APIRouter()
 
@@ -7,3 +7,4 @@ api_router.include_router(root.router)
 api_router.include_router(health.router)
 api_router.include_router(orders.router)
 api_router.include_router(products.router)
+api_router.include_router(inventory.router)
