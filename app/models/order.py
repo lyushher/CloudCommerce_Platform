b2510be723dbe3b372c5e30bdfb567ec01069f40
lysuhher <firdevszsa@gmail.com>
2026-07-27 17:42:05 +0300

    feat: add SQLAlchemy database models

---
+++ app/models/order.py
@@ -0,0 +1,81 @@
+from datetime import UTC, datetime
+from decimal import Decimal
+from uuid import UUID, uuid4
+from sqlalchemy import DateTime, Enum, ForeignKey, Integer, Numeric, String
+from sqlalchemy.dialects.postgresql import UUID as PGUUID
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from app.core.database import Base
+from app.schemas.orders import OrderStatus
+
+
+class Order(Base):
+    __tablename__ = "orders"
+
+    order_id: Mapped[UUID] = mapped_column(
+        PGUUID(as_uuid=True),
+        primary_key=True,
+        default=uuid4,
+    )
+
+    customer_name: Mapped[str] = mapped_column(
+        String(100),
+        nullable=False
+    )
+
+    total_amount: Mapped[Decimal] = mapped_column(
+        Numeric(10, 2),
+        nullable=False,
+    )
+    status: Mapped[OrderStatus] = mapped_column(
+        Enum(OrderStatus, name="order_status"),
+        nullable=False,
+        default=OrderStatus.pending,
+    )
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        nullable=False,
+        default=lambda: datetime.now(UTC),
+    )
+
+    items: Mapped[list["OrderItem"]] = relationship(
+        back_populates="order",
+        cascade="all, delete-orphan",
+    )
+
+
+class OrderItem(Base):
+    __tablename__ = "order_items"
+
+    order_item_id: Mapped[UUID] = mapped_column(
+        PGUUID(as_uuid=True),
+        primary_key=True,
+        default=uuid4,
+    )
+    order_id: Mapped[UUID] = mapped_column(
+        PGUUID(as_uuid=True),
+        ForeignKey("orders.order_id", ondelete="CASCADE"),
+        nullable=False,
+        index=True,
+    )
+    product_id: Mapped[UUID] = mapped_column(
+        PGUUID(as_uuid=True),
+        ForeignKey("products.product_id"),
+        nullable=False,
+        index=True,
+    )
+    product_name: Mapped[str] = mapped_column(
+        String(150),
+        nullable=False,
+    )
+    quantity: Mapped[int] = mapped_column(
+        Integer,
+        nullable=False,
+    )
+    price: Mapped[Decimal] = mapped_column(
+        Numeric(10, 2),
+        nullable=False,
+    )
+
+    order: Mapped["Order"] = relationship(
+        back_populates="items",
+    )
